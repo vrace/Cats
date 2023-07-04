de6d7be728b3ce74f2fa585e20763ddf3107ba6f
wane <vrace_studios@hotmail.com>
2023-07-04 22:48:13 +0800

    add context menu

--- Main.pas
+++ Main.pas
@@ -5,7 +5,7 @@ unit Main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Cat, CatRepository;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus, Cat, CatRepository;
 
 type
 
@@ -14,9 +14,20 @@ type
   TMainForm = class(TForm)
     ImageList1: TImageList;
     ListViewCats: TListView;
+    MenuItemNewCat: TMenuItem;
+    MenuItemRefreshSpace: TMenuItem;
+    MenuItemCatFeed: TMenuItem;
+    MenuItemCatPlay: TMenuItem;
+    MenuItemCatCleanse: TMenuItem;
+    PopupMenuSpace: TPopupMenu;
+    Separator1: TMenuItem;
+    MenuItemCatShow: TMenuItem;
+    PopupMenuCat: TPopupMenu;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure ListViewCatsMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure MenuItemCatShowClick(Sender: TObject);
   private
     FCatRepository: TCatRepository;
     procedure ResetListViewCats;
@@ -48,6 +59,28 @@ end;
 procedure TMainForm.FormShow(Sender: TObject);
 begin
   InvalidateListViewCats;
+end;
+
+procedure TMainForm.ListViewCatsMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if button = mbRight then begin
+    ListViewCats.Selected := ListViewCats.GetItemAt(x, y);
+    if Assigned(ListViewCats.Selected) then begin
+      PopupMenuCat.Popup(Mouse.CursorPos.X, Mouse.CursorPos.Y);
+    end else begin
+      PopupMenuSpace.PopUp(Mouse.CursorPos.X, Mouse.CursorPos.Y);
+    end;
+  end;
+end;
+
+procedure TMainForm.MenuItemCatShowClick(Sender: TObject);
+var
+  cat: TCat;
+  msg: String;
+begin
+  cat := TCat(ListViewCats.Selected.Data);
+  msg := Format('Showing cat ''%s''', [cat.Name]);
+  ShowMessage(msg);
 end;
 
 procedure TMainForm.ResetListViewCats;
